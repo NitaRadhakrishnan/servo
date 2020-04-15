f56a05390bb996086fcf8640a3d92cbc3a4555a4
Daniel Alley <dalley@redhat.com>
2020-04-14 23:15:15 -0400

    Fix resize event is not fired for window in headless mode
    fixes #15621

--- ports/glutin/window_trait.rs
+++ ports/glutin/window_trait.rs
@@ -21,8 +21,8 @@ pub trait WindowPortsMethods: WindowMethods + webxr::glwindow::GlWindow {
     fn get_fullscreen(&self) -> bool;
     fn winit_event_to_servo_event(&self, event: glutin::WindowEvent);
     fn is_animating(&self) -> bool;
+    fn set_inner_size(&self, _size: DeviceIntSize);
     fn set_title(&self, _title: &str) {}
-    fn set_inner_size(&self, _size: DeviceIntSize) {}
     fn set_position(&self, _point: DeviceIntPoint) {}
     fn set_fullscreen(&self, _state: bool) {}
     fn set_cursor(&self, _cursor: Cursor) {}
